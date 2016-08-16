2c6cdaaffde41897f042428174822379076f0382
Jesse Waites <jessewaites@Jesses-MacBook-Pro-2.local>
2016-08-16 10:21:05 -0400

    add horizontal bar chart functionality with labels and mouseover tooltips

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'test/show'
+
   resources :favorites
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
